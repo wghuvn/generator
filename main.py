b4e7b06c040b60327d082ab3aa57cb53be5eec81
wghuvn <serg.zhban@gmail.com>
2023-12-02 17:30:18 +0200

    generate

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.btn_generate.clicked.connect(self.generate)
+
+    def example(self):
+        print(1)
+
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
